29ccc87532b907ac96d100a9314108a62de8cc9d
fareedmustafa <168615588+fareedmustafa@users.noreply.github.com>
2024-08-24 00:12:55 -0400

    Update Latches.v

--- Latches.v
+++ Latches.v
@@ -1,18 +1,19 @@
-LIBRARY ieee
-USE ieee.scd logic i i €4 . all;
+LIBRARY ieee;
+USE ieee.ssd_login_1164.all;
 ENTITY latch IS
-PORT ( A : IN STD LOGIC VECTOR (7 DOWNTO 0)
-Resetn, Clock : IN STD LOGIC
-Q : OUT STD LOGIC VECTOR (7 DOWNTO 0) )
+PORT ( A : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+Resetn, Clock : IN STD_LOGIC;
+Q : OUT_STD_LOGIC VECTOR (7 DOWNTO 0) )
 END latch ;
 ARCHITECTURE Behavior OF latch IS
 BEGIN
 PROCESS ( Resetn, Clock )
 BEGIN
 IF Resetn ='0' THEN
-Q <= "00000000" •
-ELSIE Clock'EVENT AND Clock =
+Q <= "00000000" ;
+ELSEIF Clock'EVENT AND Clock = '1' THEN
+Q<=A;
 END IF;
 END PROCESS;
 END Behavior ;
-THEN
+
